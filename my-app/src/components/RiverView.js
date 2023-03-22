bc31560873a1adbd34cfa4b729df0d9d6d3ca2b4
Josh <joshuafmiller@outlook.com>
2023-03-21 22:35:36 -0400

    filter working

--- my-app/src/components/RiverView.js
+++ my-app/src/components/RiverView.js
@@ -2,13 +2,13 @@ import React from "react";
 
 
 
-const RiverCard = ({ river }) => {
+const RiverCard = ({ river, button }) => {
 //checking to see if the river is runnable, high, or low
 
-    if (river.runnable == "runnable"){
+    if ((river.runnable == "runnable" && button==3) || (river.runnable == "runnable" && button==1)){
     return (
-        
-    <div class="card m-2 text-center runnable" style={{ width: "18rem" }}>
+      
+    <div class="card m-2 text-center runnable" style={{ width: "18rem", }}>
       <div class="card-body">
         <h5 class="card-title">{river.name}</h5>
         <p class="card-text"></p>
@@ -29,7 +29,7 @@ const RiverCard = ({ river }) => {
     </div>
   );
     }
-    if (river.runnable == "high"){
+    if ((river.runnable == "high" && button==4) || (river.runnable == "high" && button==1)){
         return (
         
             <div class="card m-2 text-center high" style={{ width: "18rem" }}>
@@ -54,7 +54,7 @@ const RiverCard = ({ river }) => {
           );
     }
 
-    if (river.runnable == "low"){
+    if ((river.runnable == "low" && button==2)||(river.runnable == "low" && button==1)){
         return (
         
             <div class="card m-2 text-center low" style={{ width: "18rem" }}>
@@ -79,35 +79,35 @@ const RiverCard = ({ river }) => {
           );
     }
 
-    else {
-            return (
+    // else {
+    //         return (
             
-                <div class="card m-2 text-center" style={{ width: "18rem" }}>
-                  <div class="card-body">
-                    <h5 class="card-title">{river.name}</h5>
-                    <p class="card-text"></p>
-                  </div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                      {river.level}
-                      {river.unit}
-                    </li>
-                    <li class="list-group-item">{river.runnable}</li>
-                  </ul>
-                  <div class="card-body">
-                    <a href={river.awpage} class="card-link">
-                      American Whitewater Page8
-                    </a>
-                  </div>
-                </div>
-              );
-        }
+    //             <div class="card m-2 text-center" style={{ width: "18rem" }}>
+    //               <div class="card-body">
+    //                 <h5 class="card-title">{river.name}</h5>
+    //                 <p class="card-text"></p>
+    //               </div>
+    //               <ul class="list-group list-group-flush">
+    //                 <li class="list-group-item">
+    //                   {river.level}
+    //                   {river.unit}
+    //                 </li>
+    //                 <li class="list-group-item">{river.runnable}</li>
+    //               </ul>
+    //               <div class="card-body">
+    //                 <a href={river.awpage} class="card-link">
+    //                   American Whitewater Page8
+    //                 </a>
+    //               </div>
+    //             </div>
+    //           );
+    //     }
     };
 
 
-const RiverView = ({ data }) => {
+const RiverView = ({ data, button }) => {
   const cardData = data.map((obj, i) => {
-    return <RiverCard river={obj} key={i} />;
+    return <RiverCard river={obj} key={i} button={button} />;
   });
   return (
     <div>
