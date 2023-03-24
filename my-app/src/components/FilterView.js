f584f501027094148e82fd1071f4da907646a496
Josh <joshuafmiller@outlook.com>
2023-03-23 21:23:11 -0400

    weather modals showing data

--- my-app/src/components/FilterView.js
+++ my-app/src/components/FilterView.js
@@ -12,12 +12,12 @@ const FilterView = props => {
 
 
   return (
-    <div class= "text-center">
+    <div class= "text-center mb-2">
     <h3>Filter By:</h3>
-    <button  type="button" onClick={() => props.button(1)} class="btn btn-secondary btn-lg" style={{width: "110px"}}>All</button>
-    <button  type="button" onClick={() => props.button(2)}class="btn btn-danger btn-lg" style={{width: "110px"}}>Low</button>
-    <button  type="button" onClick={() => props.button(3)}class="btn btn-success btn-lg"style={{width: "110px"}}>Runnable</button>
-    <button  type="button" onClick={() => props.button(4)} class="btn btn-primary btn-lg"style={{width: "110px"}}>High</button>
+    <button  type="button" onClick={() => props.button(1)} class="btn btn-secondary btn-lg mx-.5" style={{width: "110px"}}>All</button>
+    <button  type="button" onClick={() => props.button(2)}class="btn btn-danger btn-lg mx-1" style={{width: "110px"}}>Low</button> 
+    <button  type="button" onClick={() => props.button(3)}class="btn btn-success btn-lg mx-.5"style={{width: "110px"}}>Runnable</button>
+    <button  type="button" onClick={() => props.button(4)} class="btn btn-primary btn-lg mx-1"style={{width: "110px"}}>High</button>
     </div>
   );
 };
--- my-app/src/components/RiverView.js
+++ my-app/src/components/RiverView.js
@@ -1,5 +1,4 @@
 import React from "react";
-import weather from "./testweather.PNG";
 import { useState, setState } from "react";
 
 const RiverCard = ({ river, button}) => {
@@ -66,9 +65,107 @@ const RiverCard = ({ river, button}) => {
                     aria-label="Close"
                   ></button>
                 </div>
-                <div class="modal-body">test</div>
-                <div class="modal-footer">
+
+                {/* modal body */}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">Current:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation: {river.weather.currentDay.precip} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.currentDay.temp)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.currentDay.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.currentDay.condition}</p>
+          </div>
                 </div>
+                        {/* day 1 forecast */}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay1.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay1.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay1.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay1.tempMin)}F-{Math.ceil(river.weather.forecastDay1.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay1.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay1.condition}</p>
+
+          </div>
+                </div>
+                {/* day 2 forecast */}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay2.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay2.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay2.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay2.tempMin)}F-{Math.ceil(river.weather.forecastDay2.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay2.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay2.condition}</p>
+
+          </div>
+                </div>
+                {/* day 3  forecast*/}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay3.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay3.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay3.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay3.tempMin)}F-{Math.ceil(river.weather.forecastDay3.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay3.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay3.condition}</p>
+
+          </div>
+                </div> 
               </div>
             </div>
           </div>
@@ -135,14 +232,115 @@ const RiverCard = ({ river, button}) => {
                     data-bs-dismiss="modal"
                     aria-label="Close"
                   ></button>
+                  </div>
+                                <div class="card m-2 text-center runnable" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">Current:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation: {river.weather.currentDay.precip} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.currentDay.temp)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.currentDay.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.currentDay.condition}</p>
+
+          </div>
                 </div>
-                <div class="modal-body">...</div>
-                <div class="modal-footer">
+                {/* copy here */}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay1.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay1.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay1.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay1.tempMin)}F-{Math.ceil(river.weather.forecastDay1.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay1.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay1.condition}</p>
+
+          </div>
                 </div>
+                {/* day 2 forecast */}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay2.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay2.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay2.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay2.tempMin)}F-{Math.ceil(river.weather.forecastDay2.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay2.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay2.condition}</p>
+
+          </div>
+                </div>
+                {/* day 3  forecast*/}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay3.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay3.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay3.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay3.tempMin)}F-{Math.ceil(river.weather.forecastDay3.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay3.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay3.condition}</p>
+
+          </div>
+                
+                
               </div>
-            </div>
+            
           </div>
         </div>
+        {/* end modal divs to copy */}
+      </div>
+      </div>
       </div>
     );
   }
@@ -203,14 +401,113 @@ const RiverCard = ({ river, button}) => {
                     aria-label="Close"
                   ></button>
                 </div>
-                <div class="modal-body">...</div>
-                <div class="modal-footer">
+                <div class="card m-2 text-center runnable" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">Current:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation: {river.weather.currentDay.precip} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.currentDay.temp)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.currentDay.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.currentDay.condition}</p>
+
+          </div>
+          
                 </div>
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay1.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay1.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay1.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay1.tempMin)}F-{Math.ceil(river.weather.forecastDay1.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay1.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay1.condition}</p>
+
+          </div>
+                </div>
+                {/* day 2 forecast */}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay2.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay2.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay2.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay2.tempMin)}F-{Math.ceil(river.weather.forecastDay2.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay2.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay2.condition}</p>
+
+          </div>
+                </div>
+                {/* day 3  forecast*/}
+                <div class="card m-2 text-center" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title fw-bolder">{river.weather.forecastDay3.date}:</h5>
+            <p class="card-text"></p>
+          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item fw-bolder">
+          Precipitation Chance: {river.weather.forecastDay3.precipChance}%
+          </li>
+          <li class="list-group-item fw-bolder">
+          Precipitation Total: {river.weather.forecastDay3.precipTotal} in
+          </li>
+          <li class="list-group-item fw-bolder">
+          Temperature: {Math.ceil(river.weather.forecastDay3.tempMin)}F-{Math.ceil(river.weather.forecastDay3.tempMax)}F
+          </li>
+        </ul>
+        <div
+          class="card-body"
+          id="runnable"
+        >
+          <img src={river.weather.forecastDay3.icon}></img>
+          <p class="fw-bold">Condition: {river.weather.forecastDay3.condition}</p>
+
+          </div>
+          </div>
               </div>
             </div>
           </div>
         </div>
+        {/* end modal divs to copy */}
       </div>
+
     );
   }
 };
